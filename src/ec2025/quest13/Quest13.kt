79ddc8160574c6f922ec823b2991e9c35172e8b0
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-11-20 08:34:40 +0100

    ec2025 q13 better

--- src/ec2025/quest13/Quest13.kt
+++ src/ec2025/quest13/Quest13.kt
@@ -1,54 +1,54 @@
 package ec2025.quest13
 
 import go
-import logged
 import provideInput
 import yearAndQuestFromPackage
 
 
 fun main() {
     val (year, quest) = yearAndQuestFromPackage({ })
+//    val ex = """
+//        10-15
+//        12-13
+//        20-21
+//        19-23
+//        30-37
+//    """.trimIndent()
+//    listOf(1, 10, 11, 12, 13, 14, 15, 20, 21, 30, 31, 32, 33, 34, 35, 36, 37, 23, 22, 21, 20, 19, 13, 12, 1)
+//        .forEachIndexed { i, n -> go("ex($i)", n) { part1(ex, i.toLong()) } }
+//
     go("part1", 797) { part1(provideInput(year, quest, 1)) }
     go("part2", 6558) { part2(provideInput(year, quest, 2)) }
     go("part3", 91492) { part3(provideInput(year, quest, 3)) }
 }
 
-fun part1(data: String): Any {
-    val ranges = data.lines().map { it.toInt() }.map { it..it }
-    return solve(ranges, 2025)
-}
+// not really ranges, but pairs (first,size)
+private fun parse(data: String): List<Pair<Long, Long>> = data.lines().map { it.split('-').map(String::toLong) }
+    .map {
+        val first = it[0]
+        val last = it.getOrElse(1) { first }
+        first to last - first + 1
+    }
 
-fun part2(data: String): Any {
-    val ranges = data.lines().map { it.split('-').let { (a, b) -> a.toInt()..b.toInt() } }
-    return solve(ranges, 20252025)
-}
+fun part1(data: String, count: Long = 2025): Long {
+    val input = parse(data)
+    val dialSize = input.sumOf { (first, size) -> size } + 1
+    val pos = count % dialSize
+    if (pos == 0L) return 1
 
-fun part3(data: String): Any {
-    val ranges = data.lines().map { it.split('-').let { (a, b) -> a.toInt()..b.toInt() } }
-    return solve(ranges, 202520252025)
-}
-
-private val IntRange.length: Int get() = (last - first + 1) / step
-private operator fun IntRange.get(i: Int): Int = first + i * step
-
-private fun solve(ranges: List<IntRange>, count: Long): Int {
-    val size = ranges.sumOf { it.length } + 1
-    val pos = (count % size).toInt()
-    if (pos == 0) return 1
-
-    var i = 1
-    var j = size - 1
+    var i = 1L // next clockwise pos
+    var j = dialSize - 1 // next counter-clockwise pos
     var clockwise = true
-    ranges.forEach { r ->
+    input.forEach { (first, size) ->
         if (clockwise) {
-            i += r.length
-            if (pos < i) return r[pos - i - r.length]
+            if (pos < i + size) return first + pos - i else i += size
         } else {
-            j -= r.length
-            if (pos > j) return r[j + r.length - pos]
+            if (pos > j - size) return first + j - pos else j -= size
         }
         clockwise = !clockwise
     }
-    error("WAT")
+    error("Not gonna happen")
 }
 
+fun part2(data: String) = part1(data, 20252025)
+fun part3(data: String) = part1(data, 202520252025)
