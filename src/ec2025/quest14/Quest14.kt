b1a6e92c0b4af8cf773924c75cbe81d1e24886a2
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-11-21 06:53:47 +0100

    ec2025 q14 p2

--- src/ec2025/quest14/Quest14.kt
+++ src/ec2025/quest14/Quest14.kt
@@ -18,35 +18,42 @@ fun main() {
     go("part3") { part3(provideInput(year, quest, 3)) }
 }
 
-fun part1(data: String) = states(parse(data)).take(10).sumOf { it.first.size }
+data class MutableState(val active: MutableSet<Pos>, val inactive: MutableSet<Pos>) {
+    val value get() = active.size.toLong()
+}
 
-private fun states(initial: Pair<MutableSet<Pos>, MutableSet<Pos>>) =
-    generateSequence(initial) { (active, inactive) ->
-        val toDeactivate = active.filter {
-            sequenceOf(it.ne(), it.nw(), it.sw(), it.se()).count { c -> c in active } % 2 == 0
-        }.toSet()
-        val toActivate = inactive.filter {
-            sequenceOf(it.ne(), it.nw(), it.sw(), it.se()).count { c -> c in active } % 2 == 0
-        }.toSet()
-        active += toActivate
-        active -= toDeactivate
-        inactive += toDeactivate
-        inactive -= toActivate
-        active to inactive
+private fun states(initial: MutableState) =
+    generateSequence(initial) { state ->
+        val toDeactivate = state.active.filter {
+            sequenceOf(it.ne(), it.nw(), it.sw(), it.se()).count { c -> c in state.active } % 2 == 0
+        }
+        val toActivate = state.inactive.filter {
+            sequenceOf(it.ne(), it.nw(), it.sw(), it.se()).count { c -> c in state.active } % 2 == 0
+        }
+        state.active += toActivate
+        state.active -= toDeactivate.toSet()
+        state.inactive += toDeactivate
+        state.inactive -= toActivate.toSet()
+        state
 //    (active + toActivate - toDeactivate) to (inactive + toDeactivate - toActivate)
     }.drop(1)
 
-private fun parse(data: String): Pair<MutableSet<Pos>, MutableSet<Pos>> {
+private fun parse(data: String): MutableState {
     val active = data.lines().findAll('#').toMutableSet()
     val inactive = data.lines().findAll('.').toMutableSet()
-    return active to inactive
+    return MutableState(active, inactive)
 }
 
-fun part2(data: String) = states(parse(data)).take(2025).sumOf { it.first.size }
+fun part1(data: String) = states(parse(data)).take(10).sumOf { it.value }
+
+fun part2(data: String) = states(parse(data)).take(2025).sumOf { it.value }
 
 fun part3(data: String): Any {
     val (active, inactive) = parse(data)
-    val initial = mutableSetOf<Pos>() to (-13..<21).flatMap { r -> (-13..<21).map { c -> Pos(r, c) } }.toMutableSet()
+    val initial = MutableState(
+        mutableSetOf(),
+        (-13..<21).flatMap { r -> (-13..<21).map { c -> Pos(r, c) } }.toMutableSet(),
+    )
     val count = 1000000000
     return states(initial).take(count).withIndex()
         .filter { (index, state) ->
@@ -54,5 +61,5 @@ fun part3(data: String): Any {
             active1.containsAll(active) && inactive1.containsAll(inactive)
         }
         .onEach { println("round ${it.index + 1}") }
-        .sumOf { it.value.first.size }
+        .sumOf { it.value.value }
 }
