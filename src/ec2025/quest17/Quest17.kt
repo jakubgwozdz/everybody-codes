c057801a67e4ce95de2f91fd6a1d7c7f7163efb2
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-11-26 07:18:14 +0100

    ec2025 q17 p3 stupid

--- src/ec2025/quest17/Quest17.kt
+++ src/ec2025/quest17/Quest17.kt
@@ -1,8 +1,15 @@
 package ec2025.quest17
 
+import coords.Direction
 import coords.findAll
+import coords.move
+import coords.pair.Pos
+import coords.pair.col
+import coords.pair.row
 import go
+import logged
 import provideInput
+import search.astar
 import yearAndQuestFromPackage
 
 
@@ -10,37 +17,138 @@ fun main() {
     val (year, quest) = yearAndQuestFromPackage({ })
     go("part1", 1560) { part1(provideInput(year, quest, 1)) }
     go("part2", 66807) { part2(provideInput(year, quest, 2)) }
-    go("part3") { part3(provideInput(year, quest, 3)) }
+    val part3ex1 = """
+        2645233S5466644
+        634566343252465
+        353336645243246
+        233343552544555
+        225243326235365
+        536334634462246
+        666344656233244
+        6426432@2366453
+        364346442652235
+        253652463426433
+        426666225623563
+        555462553462364
+        346225464436334
+        643362324542432
+        463332353552464
+    """.trimIndent()
+
+    val part3ex3 = """
+        5441525241225111112253553251553
+        133522122534119S911411222155114
+        3445445533355599933443455544333
+        3345333555434334535435433335533
+        5353333345335554434535533555354
+        3533533435355443543433453355553
+        3553353435335554334453355435433
+        5435355533533355533535335345335
+        4353545353545354555534334453353
+        4454543553533544443353355553453
+        5334554534533355333355543533454
+        4433333345445354553533554555533
+        5554454343455334355445533453453
+        4435554534445553335434455334353
+        3533435453433535345355533545555
+        534433533533535@353533355553345
+        4453545555435334544453344455554
+        4353333535535354535353353535355
+        4345444453554554535355345343354
+        3534544535533355333333445433555
+        3535333335335334333534553543535
+        5433355333553344355555344553435
+        5355535355535334555435534555344
+        3355433335553553535334544544333
+        3554333535553335343555345553535
+        3554433545353554334554345343343
+        5533353435533535333355343333555
+        5355555353355553535354333535355
+        4344534353535455333455353335333
+        5444333535533453535335454535553
+        3534343355355355553543545553345
+    """.trimIndent()
+    go("part3ex1", 592) { part3(part3ex1) }
+    go("part3ex3", 3180) { part3(part3ex3) }
+    go("part3", 48640) { part3(provideInput(year, quest, 3)) }
 }
+
+fun Pos.dist2(r: Int, c: Int) = (row - r) * (row - r) + (col - c) * (col - c)
 
 fun part1(data: String): Any {
     val grid = data.lines()
-    val (vr, vc) = grid.findAll('@').single()
-    return destroyed(grid, vc, vr, 10)
-}
-
-private fun destroyed(grid: List<String>, vc: Int, vr: Int, step: Int): Int = grid.indices.sumOf { r ->
-    grid[r].indices.sumOf { c ->
-        if ((c - vc) * (c - vc) + (r - vr) * (r - vr) in 1..step * step) grid[r][c].digitToInt() else 0
+    val volcano = grid.findAll('@').single()
+    return grid.indices.sumOf { r ->
+        grid[r].indices.sumOf { c ->
+            if (volcano.dist2(r, c) in 1..10 * 10) grid[r][c].digitToInt() else 0
+        }
     }
 }
 
 fun part2(data: String): Any {
     val grid = data.lines()
-    val (vr, vc) = grid.findAll('@').single()
-    var prev = 0
+    val volcano = grid.findAll('@').single()
     var step = 0
     val steps = buildMap {
-        while (step == 0 || prev != 0) {
+        while (true) {
             step++
-            val d = destroyed(grid, vc, vr, step)
-            put(step, d - prev)
-            prev = d
+            val range = (step - 1) * (step - 1) + 1..step * step
+            val d = grid.indices.sumOf { r ->
+                grid[r].indices.sumOf { c ->
+                    if (volcano.dist2(r, c) in range) grid[r][c].digitToInt() else 0
+                }
+            }
+            put(step, d)
+            if (d == 0) break
         }
     }
     return steps.maxBy { it.value }.let { it.key * it.value }
 }
 
 fun part3(data: String): Any {
-    return data
+    val grid = data.lines()
+    val size = (grid.size to grid.first().length).logged("size")
+    val volcano = grid.findAll('@').single().logged("volcano")
+    val start = grid.findAll('S').single().logged("start")
+    var min = 1
+    var max = volcano.row
+    var step = 1
+    while (true) {
+        step++
+        step.logged("step")
+        val totalTime = solve(grid, start, volcano, step)
+        if (totalTime >= 0) return totalTime * (step - 1)
+    }
 }
+
+fun solve(grid: List<String>, start: Pos, volcano: Pos, step: Int): Int {
+    val maxTime = (step * 30).logged("maxTime") - 1
+    val p1 = volcano.move(Direction.W, step)
+    val p2 = volcano.move(Direction.S, step)
+    val p3 = volcano.move(Direction.E, step)
+    var totalTime = cost(start, p1, grid, volcano, step)
+    totalTime.logged("totalTime after p1")
+    if (totalTime > maxTime) return -1
+    totalTime += cost(p1, p2, grid, volcano, step)
+    totalTime.logged("totalTime after p2")
+    if (totalTime > maxTime) return -1
+    totalTime += cost(p2, p3, grid, volcano, step)
+    totalTime.logged("totalTime after p3")
+    if (totalTime > maxTime) return -1
+    totalTime += cost(p3, start, grid, volcano, step)
+    totalTime.logged("totalTime after full circle")
+    if (totalTime > maxTime) return -1
+    return totalTime
+}
+
+private fun cost(start: Pos, end: Pos, grid: List<String>, volcano: Pos, step: Int): Int =
+    astar(start, end, heuristics = { 0 }) { it.neighbours(grid, volcano, step) }.drop(1).cost(grid)
+
+private fun List<Pos>.cost(grid: List<String>): Int = sumOf { (r, c) -> grid[r][c].digitToIntOrNull() ?: 0 }
+
+private fun Pos.neighbours(grid: List<String>, volcano: Pos, step: Int): List<Pair<Pos, Int>> =
+    Direction.entries.mapNotNull { d ->
+        move(d).takeIf { (r, c) ->
+            r in grid.indices && c in grid[r].indices && volcano.dist2(r, c) > (step - 1) * (step - 1)
+        }
+    }.map { it to (grid[it.row][it.col].digitToIntOrNull() ?: 0) }
