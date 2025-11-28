3a09d95cf1bb85b1180aacf20f6c66a07d9a6bc4
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2025-11-28 04:06:41 +0100

    ec2025 q19

--- src/ec2025/quest19/Quest19.kt
+++ src/ec2025/quest19/Quest19.kt
@@ -15,7 +15,7 @@ fun main() {
 // 56 first correct
 fun part1(data: String): Any {
     val walls = data.lines().map { it.split(",").map(String::toInt) }
-        .groupBy { (d, _, _) -> d }.mapValues { it.value.minBy { (_, h, _) -> h } }.map { it.value }
+        .groupBy { (d, _, _) -> d }.values.map { it.minBy { (_, h, _) -> h } }
     return walls.maxOf { (d, h, _) -> (d + 1 - h) / 2 + h }
 }
 
