e8e590cf9a65c63222509718acce9d3b2614ece5
Jakub Gwóźdź <jakub.gwozdz@gmail.com>
2026-03-06 08:02:35 +0100

    story 3

---
+++ src/ec3/quest3/Quest3.kt
@@ -0,0 +1,121 @@
+package ec3.quest3
+
+import go
+import provideInput
+import yearAndQuestFromPackage
+
+
+fun main() {
+    val (year, quest) = yearAndQuestFromPackage({ })
+    go("part1", 5371) { part1(provideInput(year, quest, 1)) }
+    go("part2", 319081) { part2(provideInput(year, quest, 2)) }
+    val ex2 = """
+        id=1, plug=RED TRIANGLE, leftSocket=BLUE TRIANGLE, rightSocket=GREEN TRIANGLE, data=?
+        id=2, plug=GREEN TRIANGLE, leftSocket=BLUE CIRCLE, rightSocket=GREEN CIRCLE, data=?
+        id=3, plug=BLUE PENTAGON, leftSocket=BLUE CIRCLE, rightSocket=GREEN CIRCLE, data=?
+        id=4, plug=RED TRIANGLE, leftSocket=BLUE PENTAGON, rightSocket=GREEN PENTAGON, data=?
+        id=5, plug=BLUE TRIANGLE, leftSocket=GREEN CIRCLE, rightSocket=RED CIRCLE, data=?
+        id=6, plug=BLUE TRIANGLE, leftSocket=GREEN CIRCLE, rightSocket=RED CIRCLE, data=?
+    """.trimIndent()
+    go("part3ex", 60) { part3(ex2) }
+    go("part3", 396978) { part3(provideInput(year, quest, 3)) }
+}
+
+typealias Plug = Pair<String, String>
+
+data class Node(
+    val id: Int,
+    val plug: Plug,
+    val leftSocket: Plug,
+    val rightSocket: Plug,
+    val data: String,
+) {
+    fun matchesWeakly(socket: Plug) = plug.first == socket.first || plug.second == socket.second
+}
+
+fun parse(data: String): List<Node> = data.reader().readLines().map { line ->
+    line.split(',').map { it.substringAfter('=') }.let { (id, plug, leftSocket, rightSocket, data) ->
+        Node(
+            id.toInt(),
+            plug.split(' ').let { (c, s) -> Plug(c, s) },
+            leftSocket.split(' ').let { (c, s) -> Plug(c, s) },
+            rightSocket.split(' ').let { (c, s) -> Plug(c, s) },
+            data
+        )
+    }
+}
+
+data class Tree(
+    var current: Node,
+    var left: Tree? = null,
+    var right: Tree? = null,
+) {
+    fun addRequireStrong(node: Node): Boolean = when {
+        left == null && node.plug == current.leftSocket -> true.also { left = Tree(node) }
+        left?.addRequireStrong(node) == true -> true
+        right == null && node.plug == current.rightSocket -> true.also { right = Tree(node) }
+        right?.addRequireStrong(node) == true -> true
+        else -> false
+    }
+
+    fun matchesWeakly(socket: Plug) = current.plug.first == socket.first || current.plug.second == socket.second
+
+    fun addAllowingWeak(node: Node): Boolean = when {
+        left == null && node.matchesWeakly(current.leftSocket) -> true.also { left = Tree(node) }
+        left?.addAllowingWeak(node) == true -> true
+        right == null && node.matchesWeakly(current.rightSocket) -> true.also { right = Tree(node) }
+        right?.addAllowingWeak(node) == true -> true
+        else -> false
+    }
+
+    fun addReplacing(branch: Tree): Tree? {
+        var b = branch
+        if (left == null && b.current.matchesWeakly(current.leftSocket)) {
+            left = b
+            return null
+        }
+        if (left != null && left?.current?.plug != current.leftSocket && b.current.plug == current.leftSocket) {
+            val t = left!!
+            left = b
+            b = t
+        } else if (left != null) b = left?.addReplacing(b) ?: return null
+        if (right == null && b.current.matchesWeakly(current.rightSocket)) {
+            right = b
+            return null
+        }
+        if (right != null && right?.current?.plug != current.rightSocket && b.current.plug == current.rightSocket) {
+            val t = right!!
+            right = b
+            b = t
+        }else if (right != null) b = right?.addReplacing(b) ?: return null
+        return b
+    }
+
+    fun flatten(): List<Node> = left?.flatten().orEmpty() + current + right?.flatten().orEmpty()
+
+    fun checksum() = flatten().mapIndexed { index, node -> (index + 1) * node.id }.sum()
+}
+
+fun part1(data: String): Any {
+    val nodes = parse(data)
+    val tree = Tree(nodes.first())
+    nodes.drop(1).forEach { tree.addRequireStrong(it) }
+    return tree.checksum()
+}
+
+fun part2(data: String): Any {
+    val nodes = parse(data)
+    val tree = Tree(nodes.first())
+    nodes.drop(1).forEach { tree.addAllowingWeak(it) }
+    return tree.checksum()
+}
+
+fun part3(data: String): Any {
+    val nodes = parse(data).map { Tree(it) }.toMutableList()
+    val tree = nodes.removeFirst()
+    while (nodes.isNotEmpty()) {
+        val it = nodes.removeFirst()
+        tree.addReplacing(it)?.let { nodes.addFirst(it) }
+    }
+    return tree.checksum()
+}
